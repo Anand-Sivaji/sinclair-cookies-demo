9ed18c6291b5fbf47965651d818d54ccf2e0cf32
ASivaji <ASivaji@USMAC1022.local>
2023-11-06 23:35:20 -0500

    Updating the IP address with the correct one

--- trendytrims/script.js
+++ trendytrims/script.js
@@ -77,7 +77,7 @@ const myImageElementWithPartitioned = document.getElementById('adBannerWithParti
 //reload ad banner
 setInterval(function() {
   var time = new Date().getMilliseconds();
-  myImageElement.src = `https://54.226.245.143.nip.io/api/get-personalized-ads?${time}`;
+  myImageElement.src = `https://3.91.29.6.nip.io/api/get-personalized-ads?${time}`;
   myImageElementWithPartitioned.src = `https://34.229.198.15.nip.io/api/get-personalized-ads?${time}`;
 }, 2000);
 
@@ -89,7 +89,7 @@ function trackUserInteraction(productViewed) {
     productName: productViewed,
     timestamp: new Date()
   };
-  const uri = 'https://54.226.245.143.nip.io/api/track-user-behavior';
+  const uri = 'https://3.91.29.6.nip.io/api/track-user-behavior';
   // Send the data to the Advertisement Site via an API call
   fetch(uri, {
     method: 'POST',
